a307bfe69740c718481af4d28b675567da6b8d0f
zavalita87 <mr086965@gmail.com>
2020-12-10 12:22:49 -0600

    tasks api v1 finished

---
+++ src/routes/tasks.routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import * as taskCtrl from "../controllers/task.controller";
+
+const router = Router();
+
+router.post("/", taskCtrl.createTask);
+
+router.get("/", taskCtrl.findAllTasks);
+
+router.get("/done", taskCtrl.findAllDoneTasks);
+
+router.get("/:id", taskCtrl.findOneTask);
+
+router.delete("/:id", taskCtrl.deleTask);
+
+router.put("/:id", taskCtrl.updateTask);
+
+export default router;
